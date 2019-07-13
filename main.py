6bbeee943e3de176b5344e2df963ec65bce88d19
Matt Wilkin <mwilkin22@gmail.com>
2019-07-13 13:21:14 -0700

    add page navigation and footer.

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    # return f"Hello, { NAME }"
+def home():
     return render_template("home.html", name=NAME)
 
 @app.route('/sales')
-def second_route():
+def sales_route():
     return render_template("sales.html", name=NAME)
+
+@app.route('/info')
+def info_route():
+    return render_template("info.html", name=NAME)
+
+@app.route('/about')
+def about_route():
+    return render_template("about.html", name=NAME)
+
+@app.route('/merch')
+def merch_route():
+    return render_template("merch.html", name=NAME)
